4c6b853bf43018605df568fa02f87e87ad3e0426
Vitor Lima Silva <110677656+Vitoroturan2905@users.noreply.github.com>
2022-11-30 09:59:09 -0300

    Add files via upload

---
+++ horadecodar2.java
@@ -0,0 +1,120 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+
+  /*exercicio 1
+System.out.println("escrva um valor");
+    Scanner scan = new Scanner(System.in);
+    int numero_usuario = scan.nextInt();
+
+    if(numero_usuario < 0){
+       System.out.println("valor negativo");
+      
+    }else if(numero_usuario == 0){
+       System.out.println("neutro");
+    }else{
+       System.out.println("valor positvo");
+    } */
+
+/*exercicio 2	
+ System.out.println("escrva o primeiro valor");
+    Scanner scan = new Scanner(System.in);
+    int valor1 = scan.nextInt();
+
+    System.out.println("escrva o segundo valor");
+    int valor2 = scan.nextInt();
+
+    System.out.println("escrva o terceiro valor");
+    int valor3 = scan.nextInt();
+
+    int resultado = Math.max(valor1, Math.max(valor2, valor3));
+
+    System.out.println("O maior é: " +resultado); */
+
+/*exercicio 3
+System.out.println("escrva o primeiro valor");
+    Scanner scan = new Scanner(System.in);
+    int valor1 = scan.nextInt();
+
+    System.out.println("escrva o segundo valor");
+    int valor2 = scan.nextInt();
+
+    System.out.println("escrva o terceiro valor");
+    int valor3 = scan.nextInt();
+
+    if(valor1 < valor2 && valor1 < valor3){
+      System.out.println("a soma é " + (valor2 + valor3));
+    }else if(valor2 < valor3 && valor2 < valor1){
+      System.out.println("a soma é " + (valor3 + valor1));
+    }else{
+      System.out.println("a soma é " + (valor2 + valor1));
+    } */
+
+/*exercicio 4
+System.out.println("escreva a nota 1");
+    Scanner scan = new Scanner(System.in);
+    int valor1 = scan.nextInt();
+
+    System.out.println("escreva a nota 2");
+    int valor2 = scan.nextInt();
+
+    System.out.println("escreva a nota 3");
+    int valor3 = scan.nextInt();
+
+    System.out.println("escreva a nota 4");
+    int valor4 = scan.nextInt();
+
+    System.out.println("escreva a nota 5");
+    int valor5 = scan.nextInt();
+
+    System.out.println("escreva a nota 6");
+    int valor6 = scan.nextInt();
+
+    System.out.println("escreva a nota 7");
+    int valor7 = scan.nextInt();
+
+    System.out.println("escreva a nota 8");
+    int valor8 = scan.nextInt();
+
+    System.out.println("escreva a nota 9");
+    int valor9 = scan.nextInt();
+
+    System.out.println("escreva a nota 10");
+    int valor10 = scan.nextInt();
+
+    int resultado = (valor1+valor2+valor3+valor4+valor5+valor6+valor7+valor8+valor9+valor10)
+/ 6;
+
+    System.out.println("a media é: " +resultado); */
+
+/*exercicio 5
+System.out.println("escreva a nota 1");
+    Scanner scan = new Scanner(System.in);
+    int valor1 = scan.nextInt();
+
+    System.out.println("escreva a nota 2");
+    int valor2 = scan.nextInt();
+
+      System.out.println("escreva a nota 3");
+    int valor3 = scan.nextInt();
+
+    System.out.println("escreva a nota 4");
+    int valor4 = scan.nextInt();
+
+    
+    int media = (valor1 + valor2 + valor3 + valor4) / 2;
+    
+    if(media >= 6){
+    System.out.println("Aprovado");
+    }else{
+    System.out.println("REPROVADO");
+    } */
+    
+
+    
+
+    
+  
+  }
+}
